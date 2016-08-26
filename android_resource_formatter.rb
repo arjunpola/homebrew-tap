c4017bc34270ff8031ce9b486bfd4ead640dd98f
Arjun Pola <arjunpola@users.noreply.github.com>
2016-08-26 16:51:23 -0700

    Update brew formula

--- android_resource_formatter.rb
+++ android_resource_formatter.rb
@@ -7,7 +7,7 @@ class AndroidResourceFormatter < Formula
   homepage ""
   url "https://github.com/arjunpola/android_resource_formatter/archive/1.0.2.tar.gz"
   version "1.0.2"
-  sha256 "883b66e6e4e05152312b23fcd658f1c6e0480a05ef2932ccb82559543cfce5b0"
+  sha256 "9e236a0d9144083bf36f2d29853f84ba3ed4bb66c69430627ae0cf8b5a744a32"
 
   def install
     bin.install "arm"

639ba5f66d86f0e590f870a61e83041d36589243
Nagarjun Pola <nagarjun.pola@ticketmaster.com>
2016-08-26 16:47:00 -0700

    update android_resource_formatter

--- android_resource_formatter.rb
+++ android_resource_formatter.rb
@@ -5,7 +5,7 @@
 class AndroidResourceFormatter < Formula
   desc ""
   homepage ""
-  url "https://github.com/arjunpola/android_resource_formatter/archive/1.0.1.tar.gz"
+  url "https://github.com/arjunpola/android_resource_formatter/archive/1.0.2.tar.gz"
   version "1.0.1"
   sha256 "883b66e6e4e05152312b23fcd658f1c6e0480a05ef2932ccb82559543cfce5b0"
 
